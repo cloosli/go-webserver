b319edcbe2e3e1a36043cb9e55bb873245753e4d
Christian Loosli <loosli.christian@gmail.com>
2016-08-11 09:26:08 +0200

    Added a bit of Complexity

---
+++ server2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func echoString(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func counter(w http.ResponseWriter, r *http.Request){
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+func main() {
+	http.HandleFunc("/", echoString)
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
